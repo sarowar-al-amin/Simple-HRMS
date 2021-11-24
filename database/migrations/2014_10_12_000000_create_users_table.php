c1059d0e527d8f3287d6255f3193c7819ad2f23f
Zishan-bs23 <saiduzzaman.zishan@brainstation-23.com>
2021-11-24 13:06:44 +0600

    modified user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,20 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('id')->unique();
+            $table->integer('role');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable(); // ?
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('active');
+            $table->boolean('benched');
+            $table->string('SBU');
+            $table->date('joining_date')->nullable();
+            $table->date('confirmation_date')->nullable();
+            $table->date('career_start_date')->nullable();
         });
     }
 
