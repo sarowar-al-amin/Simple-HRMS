08f5c9f98efe3d2bd4dd1bba66706321fe3b892a
Zishan-bs23 <saiduzzaman.zishan@brainstation-23.com>
2021-12-08 16:10:51 +0600

    datatable fixed

--- resources/views/hr/quarters/index.blade.php
+++ resources/views/hr/quarters/index.blade.php
@@ -38,32 +38,13 @@
 
 @section('js')
 
-
-{{-- <script src="../../plugins/jszip/jszip.min.js"></script> done
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script> done
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script> done
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script> done
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script> done
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script> done
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script> done
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-<!-- Page specific script -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script> done
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script> done
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script> done
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script> done
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script> done
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script> done --}}
-
 <script>
-  // $(function () {
-  //   $("#example1").DataTable({
-  //     "responsive": true, "lengthChange": false, "autoWidth": false,
-  //     "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-  //   }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-  // });
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+  });
 </script>
 
 
--- resources/views/hr/salary-reviews/index.blade.php
+++ resources/views/hr/salary-reviews/index.blade.php
@@ -38,12 +38,12 @@
 @section('js')
 
 <script>
-  // $(function () {
-  //   $("#example1").DataTable({
-  //     "responsive": true, "lengthChange": false, "autoWidth": false,
-  //     "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-  //   }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-  // });
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+  });
 </script>
 
 
