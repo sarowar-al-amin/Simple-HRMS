95d7b3506eed4526b41c4d90a1428e741eb240d2
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-01-02 18:22:51 +0600

    Modify Admin value

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,44 +11,44 @@ class UserFactory extends Factory
     {
         return [
             'id' => $this->faker->unique()->numerify('BS0Support###'),
-            'name' => $this->faker->name(),
-            'email' => 'admin@gmail.com',
+            'name' => 'Admin',
+            'email' => 'admin1.bs23xyz@gmail.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role' => 'Admin',
             'state' => 'active',
             'remember_token' => Str::random(10),
 
-            'expertise_area' => $this->faker->randomElement(['Frontend', 'Backend', 'Sales', 'Administration']),
-            'employee_type' => $this->faker->randomElement(['Developer', 'Non Developer', 'QA', 'N/A']),
-            'managerial_capacity' => $this->faker->randomElement(['Managers', 'Others', 'Self', 'N/A']),
-            'employee_category' => $this->faker->randomElement(['Contractual', 'Permanent', 'Probation']),
-            'designation' => $this->faker->randomElement(['Software Engineer', 'UI/UX Designer', 'BA', 'Trainee']),
-            'work_type' => $this->faker->randomElement(['Billable', 'Billable(investment)', 'Non-billable(L&D)', 'Non-billable(Trainee)', 'Non-billable(Bench)']),
-            'level' => $this->faker->randomElement(['IC3', 'IC3B', 'M3', 'M3B']),
+            // 'expertise_area' => $this->faker->randomElement(['Frontend', 'Backend', 'Sales', 'Administration']),
+            // 'employee_type' => $this->faker->randomElement(['Developer', 'Non Developer', 'QA', 'N/A']),
+            // 'managerial_capacity' => $this->faker->randomElement(['Managers', 'Others', 'Self', 'N/A']),
+            // 'employee_category' => $this->faker->randomElement(['Contractual', 'Permanent', 'Probation']),
+            // 'designation' => $this->faker->randomElement(['Software Engineer', 'UI/UX Designer', 'BA', 'Trainee']),
+            // 'work_type' => $this->faker->randomElement(['Billable', 'Billable(investment)', 'Non-billable(L&D)', 'Non-billable(Trainee)', 'Non-billable(Bench)']),
+            // 'level' => $this->faker->randomElement(['IC3', 'IC3B', 'M3', 'M3B']),
 
-            'sbu' => $this->faker->randomElement(['Raisul Islam', 'Support', 'Miftah Zaman']),
-            'partner' => $this->faker->randomElement(['Mizanur Rahman', 'MJ Ferdous', 'Support']),
-            'hr' => $this->faker->randomElement(['Nayem', 'Sojib', 'Siyam', 'Sohel', 'Support', 'Tori']),
-            'pm' => $this->faker->randomElement(['Nayem', 'Sojib', 'Siyam', 'Sohel', 'Support', 'Tori']),
-            'team' => $this->faker->randomElement(['GLPG', 'Hungry Naki', 'GP', 'ML', 'Support','Bench']),
-            'previous_team' => $this->faker->randomElement(['GLPG', 'Hungry Naki', 'GP', 'ML', 'Support']),
+            // 'sbu' => $this->faker->randomElement(['Raisul Islam', 'Support', 'Miftah Zaman']),
+            // 'partner' => $this->faker->randomElement(['Mizanur Rahman', 'MJ Ferdous', 'Support']),
+            // 'hr' => $this->faker->randomElement(['Nayem', 'Sojib', 'Siyam', 'Sohel', 'Support', 'Tori']),
+            // 'pm' => $this->faker->randomElement(['Nayem', 'Sojib', 'Siyam', 'Sohel', 'Support', 'Tori']),
+            // 'team' => $this->faker->randomElement(['GLPG', 'Hungry Naki', 'GP', 'ML', 'Support','Bench']),
+            // 'previous_team' => $this->faker->randomElement(['GLPG', 'Hungry Naki', 'GP', 'ML', 'Support']),
             
-            'joining_date' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
-            'confirmation_date' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
-            'career_start_date' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
+            // 'joining_date' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
+            // 'confirmation_date' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
+            // 'career_start_date' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
 
-            'blood_group' => $this->faker->randomElement(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
-            'engagement' => $this->faker->numberBetween($min=50, $max=100),
+            // 'blood_group' => $this->faker->randomElement(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
+            // 'engagement' => $this->faker->numberBetween($min=50, $max=100),
 
 
             // 'last_performance' => $this->faker->randomElement(['Need Improvement', 'Meet Expectation Very Well', 'Exceeding Expectations Heavily', 'N/A']),
             // 'last_review' => $this->faker->randomElement(['Yes', 'No', 'N/A']),
-            'comments' => $this->faker->randomElement(['very good developer', 'Lacks concentration', 'N/A']),
+            // 'comments' => $this->faker->randomElement(['very good developer', 'Lacks concentration', 'N/A']),
 
-            'plan_1' => $this->faker->randomElement(['Warning', 'Bench', 'N/A', 'L&D']),
-            'plan_2' => $this->faker->randomElement(['Warning', 'Bench', 'N/A', 'L&D']),
-            'current_status' => $this->faker->randomElement(['Bench', 'N/A', 'L&D']),
-            'available_from' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
+            // 'plan_1' => $this->faker->randomElement(['Warning', 'Bench', 'N/A', 'L&D']),
+            // 'plan_2' => $this->faker->randomElement(['Warning', 'Bench', 'N/A', 'L&D']),
+            // 'current_status' => $this->faker->randomElement(['Bench', 'N/A', 'L&D']),
+            // 'available_from' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
         ];
     }
 
