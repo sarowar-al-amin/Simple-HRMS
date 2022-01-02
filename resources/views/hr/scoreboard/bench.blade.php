f679ebef6061514ab53a43db424786e321c48f1c
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-01-02 17:11:08 +0600

    Change bench report

--- resources/views/hr/scoreboard/bench.blade.php
+++ resources/views/hr/scoreboard/bench.blade.php
@@ -12,40 +12,6 @@
     <!-- /.card-header -->
     @if ((auth()->user() && auth()->user()->role === 'Admin'))
       <div class="card-body">
-        <h3>SBU List</h3>
-        <ul>
-            <li>
-              <a href="{{url('/scoreboard/sbu/')}}"  style="border: none; background-color: white; color: blue; padding-left: 5px;">All</a>
-            </li>
-            @foreach ($name as $item)
-            <li>
-                <form action="{{url('/scoreboard/sbu/{sbu}')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <button style="border: none; background-color: white; color: blue;" name="sbu_name" type="submit" value="{{$item->sbu}}">{{$item->sbu}}</button>
-                </form>
-            </li>    
-            @endforeach
-        </ul>
-        <div class="card overflow-auto">
-          <h3>Scoreboard of all employee in the company</h3>
-          <table class="table table-hover">
-              @php
-                  $headings = ['ID', 'Name', 'Email', 'Expertise Area', 'Partner',	'Employee Type', 'Managerial Capacity',	'SBU', 'HR',
-                              'Joining Date', 'Confirmation Date', 'Career Start Date', 'Total Experience', 'Employee category',	'PM',
-                              'Blood Group', 'Designation',	'Level', 'Team', 'Actions'];
-              @endphp
-              <thead>
-                  @foreach ($headings as $heading)
-                      <th>{{ $heading }}</th>
-                  @endforeach
-              </thead>
-              <livewire:scoreboard />
-          </table>
-      </div> 
-    @else
-      <h2 align="center">You are unauthorized to access this page</h2>
-    @endif
-    <div class="card-body">
         <table id="editable" class="table table-hover">
             <thead>
                 <tr>
@@ -62,7 +28,11 @@
             </thead>
             <livewire:bench />
         </table>
-    </div>
+      </div>
+    @else
+      <h2 align="center">You are unauthorized to access this page</h2>
+    @endif
+
     <!-- /.card-body -->
 <!-- /.card -->
 @stop
