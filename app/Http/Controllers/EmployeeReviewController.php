70cecb28b812ce52c2604044c7d4b0a60b05fb1a
Zishan-bs23 <saiduzzaman.zishan@brainstation-23.com>
2022-01-04 22:07:01 +0600

    issue solve

--- app/Http/Controllers/EmployeeReviewController.php
+++ app/Http/Controllers/EmployeeReviewController.php
@@ -56,7 +56,7 @@ class EmployeeReviewController extends Controller
         $sr = SalaryReview::firstOrFail();
         $srm = SalaryReviewMetadata::where('salary_review_id', $sr->id)->where('user_id', $user->id)->firstOrFail();
         $sbu = $srm?->sbu;
-        $pm = $srm?->sbu;
+        $pm = $srm?->pm;
 
         if(Auth::user()->role === 'SBU'){
             $sbu = Auth::user()->name;
