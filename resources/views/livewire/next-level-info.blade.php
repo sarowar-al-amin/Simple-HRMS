d0dc62b2f7360fd55c301d67e97ab1566c9a540c
Zishan-bs23 <saiduzzaman.zishan@brainstation-23.com>
2022-01-04 17:03:18 +0600

    review form level collapsed form the start

--- resources/views/livewire/next-level-info.blade.php
+++ resources/views/livewire/next-level-info.blade.php
@@ -1,31 +1,23 @@
-<div class="card card-dark">
-    <div class="card-header">
-        <h3 class="card-title">{{ $level->id }}</h3>
-        <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-        </div>
-    </div>
-    <div class="card-body">
-        <table class="table table-hover">
-            @foreach ($level->toArray() as $key => $value)
-                @unless ($loop->index < 6 || $loop->index > 13)
-                    <tr>
-                        <th>{{ $key }}</th>
-                        @php
-                            $items = explode('•', $value)
-                        @endphp
-                        <td>
-                            <ul>
-                                @foreach ($items as $item)
-                                    @unless ($loop->first)
-                                        <li>{{ $item }}</li>
-                                    @endunless
-                                @endforeach
-                            </ul>
-                        </td>
-                    </tr>
-                @endunless
-            @endforeach
-        </table>
-    </div>
-</div>
+<x-adminlte-card title="{{ $level->id }}" theme="dark" collapsible="collapsed">
+    <table class="table table-hover">
+        @foreach ($level->toArray() as $key => $value)
+            @unless ($loop->index < 6 || $loop->index > 13)
+                <tr>
+                    <th>{{ ucwords($key) }}</th>
+                    @php
+                        $items = explode('•', $value)
+                    @endphp
+                    <td>
+                        <ul>
+                            @foreach ($items as $item)
+                                @unless ($loop->first)
+                                    <li>{{ $item }}</li>
+                                @endunless
+                            @endforeach
+                        </ul>
+                    </td>
+                </tr>
+            @endunless
+        @endforeach
+    </table>
+</x-adminlte-card>
