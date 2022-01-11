f8d027c9fda11441134df7599a45f2b227d83e3e
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-01-11 14:34:17 +0600

    Don't even let to see the level-hierarchy page without login --- (unwanted issue)

--- app/Http/Controllers/EmployeeLevelController.php
+++ app/Http/Controllers/EmployeeLevelController.php
@@ -1,12 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+// use Illuminate\Http\Request;
 
 class EmployeeLevelController extends Controller
 {
     public function index() {
+
+        // Without logged in traffic can't see this a bit
+        if(is_null(Auth::user())){
+            
+            return redirect('login');
+        }
+
         return view('hr.levels.index', [
             'levels' => config('employee_levels')
         ]);
