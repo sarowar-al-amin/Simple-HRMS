e3ec3333e64d6b4eecbc0b38f0b3eb428a38eeb4
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-01-17 16:48:57 +0600

    Adding searching option for reviewed employee

--- app/Models/SalaryReviewMetadata.php
+++ app/Models/SalaryReviewMetadata.php
@@ -5,11 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
 class SalaryReviewMetadata extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $guarded = [];
 
     public $timestamps = false;
+
+    public static function search($search)
+    {
+        return empty($search) ? static::query()
+            : static::query()->where('user_id', 'like', '%'.$search.'%')
+                ->orWhere('sbu', 'like', '%'.$search.'%')
+                ->orWhere('pm', 'like', '%'.$search.'%')
+                ->orWhere('promotion', 'like', '%'.$search.'%');
+    }
+
 }
