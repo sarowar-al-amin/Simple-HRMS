2bf2d3eb46f6752ed735874bfe10d5e0c7cad81b
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-03-21 19:47:58 +0600

    Given floating point to the percentage in the Bonus revew Model

--- app/Imports/BonusReview.php
+++ app/Imports/BonusReview.php
@@ -46,7 +46,7 @@ class BonusReview implements
                 'sbu' => $row['sbu_name'],
                 'pm' => $row['pm'],
                 'performance' => $row['performance_feedback'],
-                'bonus_percentage' => $row['bonus_percentage'],
+                'bonus_percentage' => $row['bonus_percentage']*100,
             ]);
         }
     }
