c3bd2ec20af75e4801ba4b9262b6c5bd9234cca9
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-03-24 14:19:40 +0600

    Updated profile update page by adding English Communication Skill updatation functionalities.

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,171 +6,171 @@
 @stop
 
 @section('content')
-<div class="container">
-    <div class="main-body">
-          <div class="row gutters-sm">
-            <div class="col-md-4 mb-3">
-              <div class="card">
-                <div class="card-body">
-                  <div class="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
-                    <div class="mt-3">
-                      <h4>{{ $user->name }}</h4>
-                      <p class="text-secondary mb-1">{{ $user->expertise_area }}</p>
-                      <p class="text-muted font-size-sm">{{ $user->email }}</p>
+    <div class="container">
+        <div class="main-body">
+            <div class="row gutters-sm">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex flex-column align-items-center text-center">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                                <div class="mt-3">
+                                    <h4>{{ $user->name }}</h4>
+                                    <p class="text-secondary mb-1">{{ $user->expertise_area }}</p>
+                                    <p class="text-muted font-size-sm">{{ $user->email }}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
+                    <div class="card mt-3">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">Website</h6>
+                                <span class="text-secondary">No idea </span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">Github</h6>
+                                <span class="text-secondary">No idea</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">Twitter</h6>
+                                <span class="text-secondary">No idea</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">Linkedin</h6>
+                                <span class="text-secondary">No idea</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">Facebook</h6>
+                                <span class="text-secondary">No idea</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card mt-3">
+                        <a class="btn btn-primary" href="{{url('profile/update')}}">
+                            Update profile
+                        </a>
+                    </div>
                 </div>
-              </div>
-              <div class="card mt-3">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">Website</h6>
-                    <span class="text-secondary">No idea </span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">Github</h6>
-                    <span class="text-secondary">No idea</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">Twitter</h6>
-                    <span class="text-secondary">No idea</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">Linkedin</h6>
-                    <span class="text-secondary">No idea</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">Facebook</h6>
-                    <span class="text-secondary">No idea</span>
-                  </li>
-                </ul>
-              </div>
-              <div class="card mt-3">
-                <a class="btn btn-primary" href="{{url('profile/update')}}">
-                    Update profile
-                </a>
-              </div>
-            </div>
-            <div class="col-md-8">
-              <div class="card mb-3">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Full Name</h6>
+                <div class="col-md-8">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-3">
+                            <h6 class="mb-0">Full Name</h6>
+                            </div>
+                            <div class="col-sm-9 text-secondary">
+                            {{ $user->name }}
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                            <h6 class="mb-0">Email</h6>
+                            </div>
+                            <div class="col-sm-9 text-secondary">
+                            {{ $user->email }}
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                            <h6 class="mb-0">Address</h6>
+                            </div>
+                            <div class="col-sm-9 text-secondary">
+                            Brain Station 23, Mohakhali, Dhaka
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                            <h6 class="mb-0">Name of SBU</h6>
+                            </div>
+                            <div class="col-sm-9 text-secondary">
+                            {{ $user->sbu }}
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                            <h6 class="mb-0">Employee Category</h6>
+                            </div>
+                            <div class="col-sm-9 text-secondary">
+                            {{ $user->employee_category }}
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                            <h6 class="mb-0">Designation</h6>
+                            </div>
+                            <div class="col-sm-9 text-secondary">
+                            {{ $user->designation }}
+                            </div>
+                        </div>
+                        <hr>
+                        </div>
                     </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{ $user->name }}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Email</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{ $user->email }}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Address</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      Brain Station 23, Mohakhali, Dhaka
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Name of SBU</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{ $user->sbu }}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Employee Category</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{ $user->employee_category }}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Designation</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      {{ $user->designation }}
-                    </div>
-                  </div>
-                  <hr>
-                </div>
-              </div>
 
-              <div class="row gutters-sm">
-                <div class="col-sm-6 mb-3">
-                  <div class="card h-100">
-                    <div class="card-body">
-                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Technical</i> Skills <a class="btn btn-primary" style="margin-left: 40%" href="#">Edit</a></h6>
-                      <small>Web Design</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Website Markup</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>One Page</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Mobile Template</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Backend API</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
+                    <div class="row gutters-sm">
+                        <div class="col-sm-6 mb-3">
+                            <div class="card h-100">
+                                <div class="card-body">
+                                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Technical</i> Skills <a class="btn btn-primary" style="margin-left: 40%" href="#">Edit</a></h6>
+                                <small>Web Design</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <small>Website Markup</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <small>One Page</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <small>Mobile Template</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <small>Backend API</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 mb-3">
+                            <div class="card h-100">
+                                <div class="card-body">
+                                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">English</i> communication skills</h6>
+                                <small>Speaking</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    @php
+                                        $value = $english_skills->speaking*10;
+                                    @endphp
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: {{$value}}%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <small>Writing</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    @php
+                                        $value = $english_skills->writing*10;
+                                    @endphp
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: {{$value}}%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <small>Listening</small>
+                                <div class="progress mb-3" style="height: 5px">
+                                    @php
+                                        $value = $english_skills->listening*10;
+                                    @endphp
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: {{$value}}%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-                <div class="col-sm-6 mb-3">
-                  <div class="card h-100">
-                    <div class="card-body">
-                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">English</i> communication skills</h6>
-                      <small>Speaking</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        @php
-                            $value = $english_skills->speaking*10;
-                        @endphp
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{$value}}%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Writing</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        @php
-                            $value = $english_skills->writing*10;
-                        @endphp
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{$value}}%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Listening</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        @php
-                            $value = $english_skills->listening*10;
-                        @endphp
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{$value}}%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
     </div>
 @stop
--- resources/views/profile/update.blade.php
+++ resources/views/profile/update.blade.php
@@ -6,35 +6,42 @@
 @stop
 
 @section('content')
-<div class="container">
-    {{-- It's working!!! --}}
-    <h3>Testing data updation. Front design isn't selected yet.</h3>
-    <div class="container-fluid page-body-wrapper">
-        <!-- partial:partials/_navbar.html -->
-            <div class="container" align="center" style="padding-top:100px;">
-                <form action="{{url('profile/info_update')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div style="padding:15px;">
-                        <label>Speaking</label>
-                        <input type="number" name="speaking" style="color:black;" placeholder="Speaking..." min="0" max="10" required="">
+    <div class="container">
+        {{-- It's working!!! --}}
+        <div class="card">
+            <!-- /.card-header -->
+            <div class="card-body">
+                <h3 align="center">Testing data updation. <br>(Front end design isn't selected yet.)</h3>
+                <div class="container-fluid page-body-wrapper">
+                    <!-- partial:partials/_navbar.html -->
+                    <div class="container" align="center" style="padding-top:100px;">
+                        <h4 align="center" style="color: lightseagreen">English Communication Skills</h4>
+                        <p>Rating range 0 to 10</p>
+                        <form action="{{url('profile/info_update')}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div style="padding:15px;">
+                                <label>Speaking</label>
+                                <input type="number" name="speaking" style="color:black;" placeholder="Speaking..." min="0" max="10" required="">
+                            </div>
+                            <div style="padding:15px;">
+                                <label>Writing</label>
+                                <input type="range" name="writing" style="color:black;" placeholder="Writing..." min="0" max="10" required="">
+                            </div>
+                            <div style="padding:15px;">
+                                <label>Listening</label>
+                                <input type="number" name="listening" style="color:black;" placeholder="Writing..." min="0" max="10" required="">
+                            </div>
+                            <div style="padding:15px;">
+                                <input type="submit" class="btn btn-primary">
+                            </div>
+                        </form>
                     </div>
-                    <div style="padding:15px;">
-                        <label>Writing</label>
-                        <input type="range" name="writing" style="color:black;" placeholder="Writing..." min="0" max="10" required="">
-                    </div>
-                    <div style="padding:15px;">
-                        <label>Listening</label>
-                        <input type="number" name="listening" style="color:black;" placeholder="Writing..." min="0" max="10" required="">
-                    </div>
-                    <div style="padding:15px;">
-                        <input type="submit" class="btn btn-primary">
-                    </div>
-                </form>
+                    <!-- main-panel ends -->
+                </div>
             </div>
-        <!-- main-panel ends -->
-         </div>
-</div>
-
+            <!-- /.card-body -->
+        </div>
+    </div>
 @stop
 
 @section('css')
