044262bcc3b60a062178394dbfc2b5c5ef1bc66a
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-03-31 14:43:13 +0600

    Add authentication from back end to the employee controller

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
     public function index() {
-        return view('employees-index');
+        if(is_null(Auth::user())){ //Make sure the loged in user is authenticated user
+            return redirect('login');
+        }elseif((Auth::user()->role == 'Admin')){
+            return view('employees-index');
+        }else{
+            return redirect()->back();
+        }
     }
 }
