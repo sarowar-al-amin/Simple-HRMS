f729b3a9be492168a5bd3ba8043f5b83b2316bb0
Zishan-bs23 <saiduzzaman.zishan@brainstation-23.com>
2022-07-04 12:03:19 +0600

    changes to review form

---
+++ app/Http/Controllers/SalaryReview22bMetadataController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SalaryReview22bMetadataController extends Controller
+{
+    public function index() {
+
+        //Authorization
+        if(is_null(Auth::user())){
+          
+            return redirect('login');
+        }
+        elseif(Auth::user()->role === 'Admin'){
+            $employees = User::orderBy('sbu')->orderBy('pm')->get();
+        }
+        else{
+            $employees = Auth::user()->role === 'SBU' ? User::where('sbu', Auth::user()->name)->get()->sortBy('pm') : User::where('pm', Auth::user()->name)->get();
+        }
+
+        
+    }
+}
--- resources/views/employee-reviews/create.blade.php
+++ resources/views/employee-reviews/create.blade.php
@@ -28,7 +28,7 @@
         <x-adminlte-profile-col-item title="Id" text="{{ $employee->id }}" size=4 />
         <x-adminlte-profile-col-item title="Employee Type" text="{{ $employee->employee_type }}" size=4 />
         <x-adminlte-profile-col-item title="SBU" text="{{ $employee->sbu }}" size=4 />
-        <x-adminlte-profile-col-item title="Experience" text="{{ $employee->experience }}" size=4 />
+        {{-- <x-adminlte-profile-col-item title="Experience" text="{{ $employee->experience }}" size=4 /> --}}
         <x-adminlte-profile-col-item title="Team" text="{{ $employee->team }}" size=4 />
         <x-adminlte-profile-col-item title="Project Maneger" text="{{ $employee->pm }}" size=4 />
         <x-adminlte-profile-col-item title="SBU" text="{{ $employee->sbu }}" size=4 />
@@ -39,10 +39,10 @@
 
 <x-adminlte-card title="Performance History" theme="dark" collapsible="collapsed">
     <x-adminlte-profile-widget>
-        <x-adminlte-profile-row-item title="Eligibility Salary Review 22A" text="{{ $employee->eligible_salary_review ?? 'N/A' }}" size="12" />
+        <x-adminlte-profile-row-item title="Eligibility Salary Review 22B" text="{{ $employee->eligible_salary_review ?? 'N/A' }}" size="12" />
         <x-adminlte-profile-row-item title="Eligibility for Q2 Bonus Review" text="{{ $employee->eligible_bonus_review ?? 'N/A' }}" size="12"/>
-        <x-adminlte-profile-row-item title="Promotional Status 21A " text="{{ $employee->second_last_promotion ?? 'N/A' }}" size="12"/>
-        <x-adminlte-profile-row-item title="Promotional Status 21B " text="{{ $employee->last_promotion ?? 'N/A' }}" size="12"/>
+        <x-adminlte-profile-row-item title="Promotional Status 22A " text="{{ $employee->second_last_promotion ?? 'N/A' }}" size="12"/>
+        <x-adminlte-profile-row-item title="Promotional Status 22A " text="{{ $employee->last_promotion ?? 'N/A' }}" size="12"/>
         <x-adminlte-profile-row-item title="Last Performance Status" text="{{ $employee->last_performance ?? 'N/A' }}" size="12"/>
     </x-adminlte-profile-widget>
 </x-adminlte-card>
