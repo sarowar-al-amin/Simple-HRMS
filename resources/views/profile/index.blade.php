4f5f7c6df20a28ffb05553ac310af4ca3c94d2a7
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-12-22 17:49:59 +0600

    [change] replace Name of SBU with SBU Head

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -84,7 +84,7 @@
                         <hr>
                         <div class="row">
                             <div class="col-sm-3">
-                            <h6 class="mb-0">Name of SBU</h6>
+                            <h6 class="mb-0">SBU Head</h6>
                             </div>
                             <div class="col-sm-9 text-secondary">
                             {{ $user->sbu }}
