3a72583a1eae9726d8bdfcbb3cdd22557a238c16
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-12-22 17:46:51 +0600

    [modify] Replace Dashboard tag to Change Password

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Change Password')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Change Password</h1>
 @stop
 
 @section('content')
@@ -14,18 +14,21 @@
         </div>
     @endif
 
-    <form id="passwordForm" action="{{ route('change-password') }}" method="POST">
+    <div class="col-6">
+        <form id="passwordForm" action="{{ route('change-password') }}" method="POST">
 
-        @csrf
-        @method('PUT')
+            @csrf
+            @method('PUT')
+    
+            <x-adminlte-input type="password" name="old_password" label="Old Password" />
+            <x-adminlte-input type="password" name="password" label="Change Password" />
+            <x-adminlte-input type="password" name="confirm_password" label="Confirm Password" />
+    
+            <x-adminlte-button class="btn btn-flat" type="submit" label="Submit" theme="dark" />
+    
+        </form>
+    </div>
 
-        <x-adminlte-input type="password" name="old_password" label="Old Password" />
-        <x-adminlte-input type="password" name="password" label="Change Password" />
-        <x-adminlte-input type="password" name="confirm_password" label="Confirm Password" />
-
-        <x-adminlte-button class="btn btn-flat" type="submit" label="Submit" theme="dark" />
-
-    </form>
 @stop
 
 @section('css')
