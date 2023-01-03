29907d59a9a31eb098f8409c4622bab2b0ec0264
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2023-01-03 13:49:47 +0600

    [create] salary-review view component with livewire

---
+++ app/Http/Livewire/EmployeeReviewFormView.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Livewire;
+use App\Models\SalaryReview;
+use App\Models\SalaryReview22bMetadata;
+use Livewire\Component;
+
+class EmployeeReviewFormView extends Component
+{
+    public $employee, $level, $presentReview;
+
+    public $review, $p_score, $p_rating, $v_score, $v_rating;
+    public $knowledge_rating, $knowledge_score, $knowledge_justification;
+    public $independence_rating, $independence_score, $independence_justification;
+    public $influence_rating, $influence_score, $influence_justification;
+    public $organizational_scope_rating, $organizational_scope_score, $organizational_scope_justification;
+    public $job_contrast_rating, $job_contrast_score, $job_contrast_justification;
+    public $execution_rating, $execution_score, $execution_justification;
+    public $ownership_rating, $ownership_score, $ownership_justification;
+    public $passion_rating, $passion_score, $passion_justification;
+    public $agility_rating, $agility_score, $agility_justification;
+    public $team_spirit_rating, $team_spirit_score, $team_spirit_justification;
+    public $honesty_rating, $honesty_score, $honesty_justification;
+    public $sbu_total_performance_rating, $sbu_total_performance_score, $sbu_promotion_recommendation, $sbu_comment;
+    public $pm_total_performance_rating, $pm_total_performance_score, $pm_promotion_recommendation, $pm_comment;
+
+    private function get_rating($score) {
+        if(is_null($score)){
+            return 'N/A';
+        } else if($score > 2) {
+            return 'Exceeding Expectation Heavily';
+        } else if($score > 1) {
+            return 'Meet Expectation Very Well';
+        } else {
+            return 'Need Improvement';
+        }
+    }
+
+    private function get_pr_score($a, $b, $c, $d, $e, $f) {
+        if(($a == 0) || ($b == 0) || ($c == 0) || ($d == 0) || ($e == 0) || ($f == 0)) return null;
+        return $a + $b + $c + $d + $e + $f;
+    }
+
+    private function get_vr_score($a, $b, $c, $d, $e) {
+        if(($a == 0) || ($b == 0) || ($c == 0) || ($d == 0) || ($e == 0)) return null;
+        return $a + $b + $c + $d + $e;
+    }
+
+    private function get_pr_rating($score) {
+        if(is_null($score)){
+            return 'Complete Remaining Selection';
+        } else if($score > 14) {
+            return 'Exceeding Expectation Heavily';
+        } else if($score > 8) {
+            return 'Meet Expectation Very Well';
+        } else {
+            return 'Need Improvement';
+        }
+    }
+
+    private function get_vr_rating($score) {
+        if(is_null($score)){
+            return 'Complete Remaining Selection';
+        } else if($score > 13) {
+            return 'Exceeding Expectation Heavily';
+        } else if($score > 7) {
+            return 'Meet Expectation Very Well';
+        } else {
+            return 'Need Improvement';
+        }
+    }
+
+
+
+    public function mount() {
+        $salary_review = SalaryReview::orderBy('start', 'desc')->first();
+        $this->presentReview = $salary_review->id;
+        $this->review = SalaryReview22bMetadata::where('user_id', $this->employee->id)->where('salary_review_id', $this->presentReview)->first();
+        
+        if($this->review) {
+            $this->knowledge_rating = $this->get_rating($this->review->knowledge_score);
+            $this->knowledge_score = $this->review->knowledge_score;
+            $this->knowledge_justification = $this->review->knowledge_justification;
+
+            $this->independence_rating = $this->get_rating($this->review->independence_score);
+            $this->independence_score = $this->review->independence_score;
+            $this->independence_justification = $this->review->independence_justification;
+
+            $this->influence_rating = $this->get_rating($this->review->influence_score);
+            $this->influence_score = $this->review->influence_score;
+            $this->influence_justification = $this->review->influence_justification;
+
+            $this->organizational_scope_rating = $this->get_rating($this->review->organizational_scope_score);
+            $this->organizational_scope_score = $this->review->organizational_scope_score;
+            $this->organizational_scope_justification = $this->review->organizational_scope_justification;
+
+            $this->job_contrast_rating = $this->get_rating($this->review->job_contrast_score);
+            $this->job_contrast_score = $this->review->job_contrast_score;
+            $this->job_contrast_justification = $this->review->job_contrast_justification;
+
+            $this->execution_rating = $this->get_rating($this->review->execution_score);
+            $this->execution_score = $this->review->execution_score;
+            $this->execution_justification = $this->review->execution_justification;
+
+            $this->ownership_rating = $this->get_rating($this->review->ownership_score);
+            $this->ownership_score = $this->review->ownership_score;
+            $this->ownership_justification = $this->review->ownership_justification;
+
+            $this->passion_rating = $this->get_rating($this->review->passion_score);
+            $this->passion_score = $this->review->passion_score;
+            $this->passion_justification = $this->review->passion_justification;
+
+            $this->agility_rating = $this->get_rating($this->review->agility_score);
+            $this->agility_score = $this->review->agility_score;
+            $this->agility_justification = $this->review->agility_justification;
+
+            $this->team_spirit_rating = $this->get_rating($this->review->team_spirit_score);
+            $this->team_spirit_score = $this->review->team_spirit_score;
+            $this->team_spirit_justification = $this->review->team_spirit_justification;
+
+            $this->honesty_rating = $this->get_rating($this->review->honesty_score);
+            $this->honesty_score = $this->review->honesty_score;
+            $this->honesty_justification = $this->review->honesty_justification;
+
+            $this->sbu_total_performance_rating = $this->review->sbu_total_performance_rating;
+            $this->sbu_total_performance_score = $this->review->sbu_total_performance_score;
+            $this->sbu_promotion_recommendation = $this->review->sbu_promotion_recommendation;
+            $this->sbu_comment = $this->review->sbu_comment;
+
+            $this->pm_total_performance_rating = $this->review->pm_total_performance_rating;
+            $this->pm_total_performance_score = $this->review->pm_total_performance_score;
+            $this->pm_promotion_recommendation = $this->review->pm_promotion_recommendation;
+            $this->pm_comment = $this->review->pm_comment;
+        }
+    }
+    public function render()
+    {
+        $this->p_score = $this->get_pr_score($this->knowledge_score, $this->independence_score, $this->influence_score, $this->organizational_scope_score, $this->job_contrast_score, $this->execution_score);
+        $this->p_rating = $this->get_pr_rating($this->p_score);
+        $this->v_score = $this->get_vr_score($this->passion_score, $this->ownership_score, $this->agility_score, $this->team_spirit_score, $this->honesty_score);
+        $this->v_rating = $this->get_vr_rating($this->v_score);
+        $this->sbu_promotion_recommendation;
+        return view('livewire.employee-review-form-view');
+    }
+}
---
+++ resources/views/livewire/employee-review-form-view.blade.php
@@ -0,0 +1,224 @@
+<form id="reviewForm" action={{ route('employee-salary-reviews.store', ['user' => $employee]) }} method="POST" encType="multipart/form-data">
+
+    @csrf
+
+    @php
+        $headers = ['Category', 'Indicators', 'Rating', 'Justification'];
+        $ratings = ['Select An Option','Need Improvement', 'Meet Expectation Very Well', 'Exceeding Expectation Heavily'];
+    @endphp
+
+    <h1>Performance Rating</h1>
+
+    <table class="table table-hover">
+
+        @php
+            $performance_categories = ['Knowledge/Experience', 'Independence', 'Influence', 'Organizational Scope', 'Job Contrast/Complexity', 'Execution'];
+            $performance_names = ['knowledge', 'independence', 'influence', 'organizational_scope', 'job_contrast', 'execution'];
+            $performance_indicators = [$level->knowledge_questions, $level->independence_questions, $level->influence_questions, $level->organizational_scope_questions, $level->job_contrast_questions, $level->execution_questions];
+        @endphp
+    
+        {{-- Headers --}}
+        <thead>
+            @foreach ($headers as $header)
+                <th>
+                    {{ $header }} @if ($loop->index == 2) <span class="required_color">*</span> @endif
+                </th>
+            @endforeach
+        </thead>
+
+        <tbody>
+            @for ($i=0; $i<6; $i++)
+                <tr>
+                    <td>
+                        {{ $performance_categories[$i] }}
+                    </td>
+                    <td>
+                        {{ $performance_indicators[$i] }}
+                    </td>
+                    <td id='pr1'>
+                        <select name={{ $performance_names[$i].'_score' }} wire:model={{ $performance_names[$i].'_score' }}>
+                            @foreach ($ratings as $rating)
+                                @if ($loop->index == 0)
+                                    <option value={{ $loop->index }}>{{ $rating }}</option>
+                                @else
+                                    <option value={{ $loop->index }}>{{ $rating }}</option>
+                                @endif
+                                    {{-- <option value={{ $loop->index }}>{{ $rating }}</option> --}}
+                            @endforeach
+                        </select>
+                    </td>
+
+                    <td>
+                        <textarea name={{ $performance_names[$i].'_justification' }} wire:model={{ $performance_names[$i].'_justification' }} placeholder="If you may, you can justify your action."></textarea>
+                    </td>
+                </tr>
+            @endfor
+            <tr>
+                <td>
+                    <h3>Overall</h3>
+                </td>
+                <td>
+                    <h3 class="font-weight-bold">{{ $p_rating }}</h3>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <h3>Values Rating</h3>
+
+    <table class="table table-hover">
+
+        @php
+            $values_categories = ['Ownership', 'Passion & Commitment', 'Agility & Exellence', 'Team Sprit', 'Honesty'];
+            $values_names = ['ownership', 'passion', 'agility', 'team_spirit', 'honesty'];
+            $values_indicators = [
+                "<strong>Take responsibility and own the challenges</strong>
+                <ul>
+                <li>Client partnership - We consistently exceed client expectations and suggest better solutions.</li>
+                <li>Grow people - We hire, develop and care for people, being the best employer for them.</li>
+                <li>Organizational ownership - we love our organization and are frugal, innovative, and simplified for better results. Organizational success brings success for all.</li>
+                </ul>",
+                
+                "<strong>Be reliable, dedicated, and smart working</strong>
+                <ul>
+                <li>We love what we do, and we do what we love.</li>
+                <li>Passion enables us to learn and be curious.</li>
+                <li>We strive to deliver what we commit, thus earning trust and respect.</li>
+                </ul>",
+
+                "<strong>Delivers the best solutions, and stay agile beyond boundaries</strong>
+                <ul>
+                <li>We strive for excellence; we lead by example.</li>
+                <li>plan agile, deliver results in a short cycle, and make continuous improvements.</li>
+                <li>We strive relentlessly, and we take action for continuous improvement.</li>
+                </ul>",
+
+                "<strong>Be humble and value relationships</strong>
+                <ul>
+                <li>We stay humble and work as a team. Humility and empathy are at our core.</li>
+                <li>success brings broader responsibilities and challenges. We live that expectation.</li>
+                <li>We value relationships with our clients, employees, shareholders, and society; we continuously build on them</li>
+                </ul>",
+
+                "<strong>Say what you think and do what you say</strong>
+                <ul>
+                <li>all our actions, we are always ethical and sincere.</li>
+                <li>share our views and concerns, even in disagreements - we strive to be the trusted advisor to our clients and colleagues.</li>
+                <li>are admired and respected for our integrity and fairness.</li>
+                </ul>"
+            ];
+        @endphp
+
+        <thead>
+            @foreach ($headers as $header)
+                <th>
+                    {{ $header }}@if ($loop->index == 2) <span class="required_color">*</span> @endif
+                </th>
+            @endforeach
+        </thead>
+
+        <tbody>
+            @for ($i=0; $i<5; $i++)
+                <tr>
+                    <td>
+                        {{ $values_categories[$i] }}
+                    </td>
+                    <td>
+                        {!! $values_indicators[$i] !!}
+                    </td>
+                    <td id='vr'>
+                        <select name={{ $values_names[$i].'_score' }} wire:model={{ $values_names[$i].'_score' }}>
+                            @foreach ($ratings as $rating)
+                                @if ($loop->index == 0)
+                                    <option value={{ $loop->index }}>{{ $rating }}</option>
+                                @else
+                                    <option value={{ $loop->index }}>{{ $rating }}</option>
+                                @endif
+                                    {{-- <option value={{ $loop->index }}>{{ $rating }}</option> --}}
+                            @endforeach
+                        </select>
+                    </td>
+                    <td>
+                        <textarea name={{ $values_names[$i].'_justification' }} wire:model={{ $values_names[$i].'_justification' }} placeholder="If you may, you can justify your action."></textarea>
+                    </td>
+                </tr>
+            @endfor
+            <tr>
+                <td>
+                    <h3>Overall</h3>
+                </td>
+                <td>
+                    <h3 class="font-weight-bold">{{ $v_rating }}</h3>
+                </td>
+                {{-- <td>
+                    <h3 id="vo_score" class="font-weight-bold"></h3> 
+                </td> --}}
+            </tr>
+        </tbody>
+
+    </table>
+
+    <h1>Overall Rating</h1>
+
+    <table class="table tavle-hover">
+        <tbody>
+            <tr>
+                <td>Overall Performance</td>
+                <td>{{ $p_rating }}</td>
+            </tr>
+            <tr>
+                <td>Values Rating</td>
+                <td>{{ $v_rating }}</td>
+            </tr>
+            
+            @if (auth()->user()->role == 'SBU' || auth()->user()->role == 'Admin')
+            <tr>
+                <td>Recommended for Promotion</td>
+                <td>
+                    <select id="promote" name='promotion recommendation'>
+                        <option disabled {{ is_null($review) ? 'selected': '' }}>Select An Option</option>
+                        @foreach (['Yes', 'No'] as $rating)
+                            <option value={{ $rating }} {{ $review && ($review['sbu_promotion_recommendation'] === $rating || $review['pm_promotion_recommendation'] === $rating) ? 'selected' : '' }}>{{ $rating }}</option>
+                        @endforeach
+                    </select>
+                </td>
+            </tr>
+            @else
+            <tr>
+                <td>Recommended for Promotion</td>
+                <td>
+                    <select id="promote" name='promotion recommendation'>
+                        <option disabled {{ is_null($review) ? 'selected': '' }}>Select An Option</option>
+                        @foreach (['Yes', 'No'] as $rating)
+                            <option value={{ $rating }} {{ $review && $review['pm_promotion_recommendation'] === $rating  ? 'selected' : '' }}>{{ $rating }}</option>
+                        @endforeach
+                    </select>
+                </td>
+            </tr>
+            @endif
+
+            @if (auth()->user()->role != 'PM')
+
+                <tr>
+                    <td>SBU Comment <span class="required_color">*</span></td>
+                    <td>
+                        <textarea required="true" name="sbu_comment" cols="80" rows="4" placeholder='Your comment is necessary' wire:model='sbu_comment'></textarea>
+                    </td>
+                </tr>
+                
+            @endif
+            <tr>
+                <td>PM Comment <span class="required_color">*</span></td>
+                <td>
+                    <textarea required="true" name="pm_comment" cols="80" rows="4" placeholder='Your comment is necessary' wire:model='pm_comment'></textarea>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <div class="d-flex justify-content-between p-4">
+        <x-adminlte-button class="btn btn-lg" onclick="history.back()" label="Back" theme="danger" />
+        <x-adminlte-button class="btn btn-lg disabled" label="Submit" theme="success" />
+          
+    </div>
+</form>
